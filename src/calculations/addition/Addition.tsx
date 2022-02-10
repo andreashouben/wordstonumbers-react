58d80b4f36529439de3a5407580b8581d2d3c307
Andreas Houben <andreas.houben@codecentric.de>
2022-02-10 16:24:03 +0100

    fix lint error

--- src/calculations/addition/Addition.tsx
+++ src/calculations/addition/Addition.tsx
@@ -20,7 +20,7 @@ export const Addition = ({ digits }: AdditionProps) => {
 
   let crossfoots1 = crossfoots(sum);
   let cfText = "";
-  if (crossfoots1.length == 1) {
+  if (crossfoots1.length === 1) {
     cfText = `(QS: ${renderCalculation(crossfoots1[0])})`;
   } else if (crossfoots1.length > 1) {
     const sums = crossfoots1.map((cf) => renderCalculation(cf)).join("; ");
